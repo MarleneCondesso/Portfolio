8175073f6f627372fea24e2336c55d3794897d91
MarleneCondesso <marlene.condesso23@gmail.com>
2023-05-03 21:11:25 +0100

    add icon

--- portfolio/pages/_app.tsx
+++ portfolio/pages/_app.tsx
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <title>Marlene Condesso | Portfolio</title>
             <meta name='description' />
             <link rel='icon' href='/images/logoDarkTab.png' className='h-[400px]'/>
+            <link rel="apple-touch-icon" href='/images/logoDarkTab.png' className='h-[400px]'/>
           </Head>
           <ThemeProvider>
             {/**<NextNProgress options={{ easing: 'ease', speed: 500 }}/>**/}
